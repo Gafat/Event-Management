fde27675d271253e78488c06099ed9a3ba5c5606
kalkidanf <promiseyet@gmail.com>
2015-03-04 21:48:07 -0800

    Event Management

---
+++ EventManagement/src/gafat/service/WeddingService.java
@@ -0,0 +1,8 @@
+package gafat.wedding.service;
+
+import java.util.List;
+
+public interface WeddingService {
+	public List<String> getCountryNameList();
+
+}
---
+++ EventManagement/src/gafat/service/WeddingServiceImpl.java
@@ -0,0 +1,24 @@
+package gafat.wedding.service;
+
+import gafat.wedding.repository.WeddingRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+public class WeddingServiceImpl implements WeddingService {
+	
+	@Autowired 
+	WeddingRepository weddinRepository;
+	
+	
+	public List<String> getCountryNameList()
+	{
+		List<String> countryNameList=new ArrayList();
+		countryNameList=weddinRepository.getCountryNameList();
+		return countryNameList;		
+	}
+
+}
